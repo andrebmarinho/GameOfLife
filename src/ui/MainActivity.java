0e26abf44b532980c57f83e4f881917d73e059e5
andrebmarinho <andrebezerramarinho@gmail.com>
2014-12-05 20:37:41 -0200

    Interface quase pronta
    falta implementar botões e aplicar as próximas gerações no jogo.

--- src/ui/MainActivity.java
+++ src/ui/MainActivity.java
@@ -35,27 +35,21 @@ public class MainActivity extends Activity {
        gridView.setAdapter(adapter);
        
        Main.instance().startGame();
+       controller = Main.instance().getController();
        
        gridView.setOnTouchListener(new OnTouchListener() {
            @SuppressLint("ClickableViewAccessibility") public boolean onTouch(View v, MotionEvent me) {
                
-               int position = gridView.pointToPosition((int) me.getX(), (int) me.getY());
-               ImgSwitch.instance().setImgArray(Status.Alive, position); 
+        	   int x = (int) me.getX();
+        	   int y = (int) me.getY();               
+               int[] coord = ImgSwitch.instance().convertPositionToCoordinates( gridView.pointToPosition(x, y) );
+               controller.makeCellAlive(coord[0], coord[1]);
                updateView();
                return true;
            }
 		
        });
-       
-       
-     //Isso na verdade vai no loop infinito do jogo.
-       /*controller = Main.instance().getController();
-       
-       if (controller.getUpdate())
-			updateView();*/
-       
-       
-       
+              
     }
 	
 	private void updateView() {
